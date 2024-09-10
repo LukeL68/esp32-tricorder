dfe66bf0fd01e211b51f9eb4f898586ba5edddcb
Luke <luke.landry468@gmail.com>
2024-09-10 09:40:51 -0400

    Initial commit

---
+++ main/gatt_server.c
@@ -0,0 +1,18 @@
+
+#include "host/ble_uuid.h"
+#include "services/gatt/ble_svc_gatt.h"
+
+
+// // Environmental sensing service and characteristics UUID values are from Bluetooth standard
+// // https://www.bluetooth.com/wp-content/uploads/Files/Specification/HTML/Assigned_Numbers/out/en/Assigned_Numbers.pdf?v=1725972233968
+
+// static const ble_uuid16_t environmental_sensing_service_uuid = BLE_UUID16_INIT(0x181A);
+
+// static const ble_uuid16_t pressure_characteristic_uuid = BLE_UUID16_INIT(0x2A6D);
+// static const ble_uuid16_t temperature_characteristic_uuid = BLE_UUID16_INIT(0x2A6E);
+// static const ble_uuid16_t humidity_characteristic_uuid = BLE_UUID16_INIT(0x2A6F);
+
+// uint32_t gattserver_pressure_val;
+// int16_t gattserver_temperature_val;
+// uint16_t gattserver_humidity_val;
+
